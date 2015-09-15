37c0a149b606c6c730a88d32c3e8946d64294f4f
brianpenghe <brianpenghe@gmail.com>
2015-09-15 00:30:26 -0700

    removed debug echo; added LocalCodeGenerator file with if conditions to catch fastq/fastq.gz issues

--- LocalCodeGeneratorATAC.sh
+++ LocalCodeGeneratorATAC.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Run these codes in the current SERVER directory
 #the file $test has two columns, link and name, only one space allowed
+#each experiment folder contains a set of fastq files belonging to the same library
+# for example: /woldlab/castor/home/phe/ChIPseq   test1
 #delete the testFolderPath file
 #it generates three files: bowtieXXXXXX testcode & testFolderPath
 #usage: ~/programs/GECCodeGeneratorATAC.sh test mm9 36
@@ -8,7 +10,6 @@
 echo '' > testcode
 CurrentLo=$(pwd)
 source /woldlab/castor/home/phe/programs/GenomeDefinitions.sh $2
-/woldlab/castor/home/phe/programs/DownloadFolder.sh $1
 
 echo '' >> testcode
 echo "******take a break***********" >> testcode
@@ -16,15 +17,16 @@ echo "refolder,unzip and FastQC codes:" >> testcode
 echo "********(checkout bowtie condor file)*********" >> testcode
 while read line
     do
-        Folders=$(echo $line | cut -d' ' -f1 | sed "s/https:\///g" | rev | cut -d '/' -f3- | rev)
-        SampleID=$(echo $line | cut -d' ' -f1 | rev | cut -d '/' -f2 | rev)
+        Folders=$(echo $line | cut -d' ' -f1 | rev | cut -d '/' -f2- | rev)
+        SampleID=$(echo $line | cut -d' ' -f1 | rev | cut -d '/' -f1 | rev)
         SampleMeta=$(echo $line | cut -d' ' -f2- | sed "s/\//_/g" | sed "s/ /_/g")
-        OldDataPath=$(echo $CurrentLo$Folders"/"$SampleID)
+        OldDataPath=$(echo $Folders"/"$SampleID)
         path=$(echo $CurrentLo"/"$SampleID$SampleMeta)
         printf "mv "$OldDataPath" "$path" && " >> testcode
         printf $path"\n" >> testFolderPath
         printf "mkdir "$path"FastQCk6 && " >> testcode
-        printf "gunzip -c "$path"/*.fastq.gz | python /woldlab/castor/home/georgi/code/trimfastq.py - "$3" -stdout > "$path"allfastq && " >> testcode
+        printf "cat "$path"/*.fastq | python /woldlab/castor/home/georgi/code/trimfastq.py - "$3" -stdout > "$path"allfastq ; " >> testcode
+        printf "if [ -s "$path"allfastq ] ; then echo 'q' ; else ; gunzip -c "$path"/*.fastq.gz | python /woldlab/castor/home/georgi/code/trimfastq.py - "$3" -stdout > "$path"allfastq ; fi ; " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$path"allfastq -o "$path"FastQCk6 -k 6 & \n" >> testcode
     done <$1
 
