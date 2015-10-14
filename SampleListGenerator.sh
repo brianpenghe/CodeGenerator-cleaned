da12f756f1cab48517b550b5fd628703d177650d
brianpenghe <brianpenghe@gmail.com>
2015-10-13 22:43:07 -0700

    fixed the bug of if condition problem: the server asks for a space between [ and the content

--- SampleListGenerator.sh
+++ SampleListGenerator.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SampleListGenerator.sh input output
 printf '' > $2
+wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/ -q -O - > index.html
 while read line
     do
-        if [$(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | wc -l) != 0 ]
+        if [ $(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | wc -l) != 0 ]
             then
                 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | cut -d/ -f3 > Flowcell
                 while read Flow
                     do
                         folder=$(grep $Flow index.html | grep -v $Flow"_temp" | cut -d"\"" -f8)
-                        if [$(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/$folder"Unaligned/" -q -O - | grep $line | wc -l) == 0 ]
+                        if [ $(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/$folder"Unaligned/" -q -O - | grep $line | wc -l) == 0 ]
                             then
                                 printf $line" no "$Flow" data found\n"
                                 continue
