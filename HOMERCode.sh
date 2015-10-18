a5eafbc09113ba8e82f5d71f4c1405c2823343d0
brianpenghe <brianpenghe@gmail.com>
2015-10-18 00:34:59 -0700

    added bibbed feature for HOMERscript

--- HOMERCode.sh
+++ HOMERCode.sh
@@ -21,7 +21,8 @@ while read line
         printf "condor_run \"python /woldlab/castor/home/georgi/code/SAMstats.py "$line"."$2"."$3".unique.nochrM.bam "$line"."$2"."$3".unique.nochrM.SAMstats -bam "$chromsizes" /woldlab/castor/proj/programs/samtools-0.1.8/samtools \" && " >> testcode
         printf "condor_run \" /woldlab/castor/proj/programs/homer-4.7/bin/makeTagDirectory "$line"."$2"."$3"HomerTags "$line"."$2"."$3".unique.nochrM.bam \" && " >> testcode
         printf "condor_run \"/woldlab/castor/proj/programs/homer-4.7/bin/findPeaks "$line"."$2"."$3"HomerTags -localSize 50000 -minDist 50 -size 150 -fragLength 0 -o "$line"."$2"."$3"lS50000mD50s150fL0 2> "$line"."$2"."$3"lS50000mD50s150fL0.err \" && " >> testcode
-        printf "grep 000 "$line"."$2"."$3"lS50000mD50s150fL0 | grep chr - | grep -v = | awk '{print \$2\"\\\t\"\$3\"\\\t\"\$4\"\\\t\"\$1\"\\\t225\\\t\"\$5}' - | sort -k 1d,1 -k 2n,2 > "$line"."$2"."$3"lS50000mD50s150fL0.bed & " >> testcode
+        printf "grep 000 "$line"."$2"."$3"lS50000mD50s150fL0 | grep chr - | grep -v = | awk '{print \$2\"\\\t\"\$3\"\\\t\"\$4\"\\\t\"\$1\"\\\t225\\\t\"\$5}' - | sort -k 1d,1 -k 2n,2 > "$line"."$2"."$3"lS50000mD50s150fL0.bed && " >> testcode
+        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3"lS50000mD50s150fL0.bed "$line"."$2"."$3"lS50000mD50s150fL0.bigBed & \n" >> testcode
     done <$1
 
 
@@ -35,7 +36,7 @@ printf "current_folder_name=\$(pwd|rev|cut -d '/' -f1|rev)" >> testcode
 printf '''
     for file in *.bigBed
         do
-        echo "track type=bigBed name="$file" description="$file" maxHeightPixels=60:32:8 visibility=dense color=150,0,150 bigDataUrl=http://woldlab.caltech.edu/~phe/"$current_folder_name"/"$file >> testcode
+        echo "track type=bigBed name="$file" description="$file" maxHeightPixels=60:32:8 visibility=dense color=150,0,150 bigDataUrl=http://woldlab.caltech.edu/~phe/"$current_folder_name"/"$file >> tracksummary
         done
 ''' >> testcode
 
