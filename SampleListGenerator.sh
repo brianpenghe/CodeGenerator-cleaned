6579d3e24f256006a6652d02953c2519df820908
brianpenghe <brianpenghe@gmail.com>
2015-10-17 23:58:01 -0700

    modified standard error message

--- SampleListGenerator.sh
+++ SampleListGenerator.sh
@@ -4,6 +4,7 @@ printf '' > $2
 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/ -q -O - > index.html
 while read line
     do
+        printf $line":"
         if [ $(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | wc -l) != 0 ]
             then
                 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | cut -d/ -f3 > Flowcell
@@ -12,15 +13,16 @@ while read line
                         folder=$(grep $Flow index.html | grep -v $Flow"_temp" | cut -d"\"" -f8)
                         if [ $(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/$folder"Unaligned/" -q -O - | grep $line | wc -l) == 0 ]
                             then
-                                printf $line" no "$Flow" data found\n"
+                                printf " no "$Flow" data found\n"
                                 continue
                         else
                             project=$(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/$folder"Unaligned/" -q -O - | grep $line | cut -d"\"" -f8)
                             printf https://jumpgate.caltech.edu/runfolders/volvox/$folder"Unaligned/"$project"Sample_"$line"/ " >> $2
+                            printf " got "$Flow"\n"
                         fi
                     done<Flowcell
                 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep libns:name | cut -d"<" -f2 | cut -d">" -f2 >> $2
         else
-            printf $line" no Flowcells found\n"
+            printf " no Flowcells found\n"
         fi
     done <$1
