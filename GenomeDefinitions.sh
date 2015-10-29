44037bf8adc55cd9821d845f99a7195e7d26502c
brianpenghe <brianpenghe@gmail.com>
2015-10-28 21:54:30 -0700

    Added blacklist feature

--- GenomeDefinitions.sh
+++ GenomeDefinitions.sh
@@ -8,22 +8,31 @@ then
     fa="/woldlab/castor/proj/genome/bowtie-indexes/mm9.fa"
     bowtieindex="/woldlab/castor/proj/genome/bowtie-indexes/mm9"
     chromsizes="/woldlab/castor/home/georgi/genomes/mm9/mm9.chrom.sizes"
+    blacklist="/woldlab/castor/home/phe/genomes/mm9/mm9-blacklist.bed"
+    mitoblack="/woldlab/castor/home/phe/genomes/mm9/chrM30mermm9lS50000mD50s150fL0.bed"
+    
 elif [ "$1" == "mm10" ]
 then
-fa="/woldlab/castor/proj/genome/bowtie-indexes/mm10.fa"
-bowtieindex="/woldlab/castor/proj/genome/bowtie-indexes/mm10"
-chromsizes="/woldlab/castor/home/georgi/genomes/mm10/mm10.chrom.sizes"
+    fa="/woldlab/castor/proj/genome/bowtie-indexes/mm10.fa"
+    bowtieindex="/woldlab/castor/proj/genome/bowtie-indexes/mm10"
+    chromsizes="/woldlab/castor/home/georgi/genomes/mm10/mm10.chrom.sizes"
+    blacklist="/woldlab/castor/home/phe/genomes/mm10/mm10blacklist_Ricardo.bed"
+    mitoblack="/woldlab/castor/home/phe/genomes/mm10/chrM30mermm10lS50000mD50s150fL0.bed"
 
 elif [ "$1" == "hg19male" ]
 then
     fa="/woldlab/castor/proj/genome/bowtie-indexes/ENCFF001RGR+spikes.fa"
     bowtieindex="/woldlab/castor/proj/genome/bowtie-indexes/ENCFF001RGR+spikes"
     chromsizes="/woldlab/castor/home/georgi/genomes/hg19/hg19-male-single-cell-NIST-fixed-spikes.chrom.sizes"
+    blacklist="/woldlab/castor/home/phe/genomes/hg19/blacklist/wgEncodeDacMapabilityConsensusExcludable.bed"
+    mitoblack="/woldlab/castor/home/phe/genomes/hg19/blacklist/blacklist_.hg19male.30merlS50000mD50s150fL0.bed"
 elif [ "$1" == "hg19female" ]
 then
     fa="/woldlab/castor/proj/genome/bowtie-indexes/ENCFF001RGS+spikes.fa"
     bowtieindex="/woldlab/castor/proj/genome/bowtie-indexes/ENCFF001RGS+spikes"
     chromsizes="/woldlab/castor/home/georgi/genomes/hg19/hg19-female-single-cell-NIST-fixed-spikes.chrom.sizes"
+    blacklist="/woldlab/castor/home/phe/genomes/hg19/blacklist/wgEncodeDacMapabilityConsensusExcludable.bed"
+    mitoblack="/woldlab/castor/home/phe/genomes/hg19/blacklist/blacklist_.hg19male.30merlS50000mD50s150fL0.bed"
 fi
 
 
--- eRangeCode.sh
+++ eRangeCode.sh
@@ -23,8 +23,10 @@ while read line
         printf "python /woldlab/castor/home/georgi/code/commoncode/findall.py "$line"."$2"."$3".unique.nochrM.5x.4RPM- "$line"."$2"."$3".unique.nochrM.rds "$line"."$2"."$3".unique.nochrM.5x.4RPM.hts -minimum 4 -ratio 5 -listPeak -cache 20000000 -nodirectionality && rm "$line"."$2"."$3".unique.nochrM.rds && rm "$line"."$2"."$3".unique.nochrM.rds.log && " >> testcodeeRange
         printf "python /woldlab/castor/home/georgi/code/commoncode/regiontobed.py --- "$line"."$2"."$3".unique.nochrM.3x.2RPM.hts "$line"."$2"."$3".unique.nochrM.3x.2RPM.bed -nolabel && " >> testcodeeRange
         printf "python /woldlab/castor/home/georgi/code/commoncode/regiontobed.py --- "$line"."$2"."$3".unique.nochrM.5x.4RPM.hts "$line"."$2"."$3".unique.nochrM.5x.4RPM.bed -nolabel && " >> testcodeeRange
-        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.3x.2RPM.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.3x.2RPM.bigBed && " >> testcodeeRange
-        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.5x.4RPM.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.5x.4RPM.bigBed & \n" >> testcodeeRange
+        printf "sort -k 1d,1 -k 2n,3 "$line"."$2"."$3".unique.nochrM.3x.2RPM.bed | intersectBed -a - -b "$mitoblack" -v | intersectBed -a - -b "$blacklist" -v > "$line"."$2"."$3".unique.nochrM.3x.2RPM.clean.bed && mv "$line"."$2"."$3".unique.nochrM.3x.2RPM.clean.bed "$line"."$2"."$3".unique.nochrM.3x.2RPM.bed &&" >> testcodeeRange
+        printf "sort -k 1d,1 -k 2n,3 "$line"."$2"."$3".unique.nochrM.5x.4RPM.bed | intersectBed -a - -b ~/genomes/mm9/mm9-blacklist.bed -v | intersectBed -a - -b "$mitoblack" -v | intersectBed -a - -b "$blacklist" -v > "$line"."$2"."$3".unique.nochrM.5x.4RPM.clean.bed && mv "$line"."$2"."$3".unique.nochrM.5x.4RPM.clean.bed "$line"."$2"."$3".unique.nochrM.5x.4RPM.bed &&" >> testcodeeRange
+        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.3x.2RPM.clean.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.3x.2RPM.bigBed && " >> testcodeeRange
+        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.5x.4RPM.clean.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.5x.4RPM.bigBed & \n" >> testcodeeRange
     done <$1
 
 
