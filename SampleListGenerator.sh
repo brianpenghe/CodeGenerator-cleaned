f6ba04de540e1ca17c048a776281b471f2306180
brianpenghe <brianpenghe@gmail.com>
2015-11-10 11:44:32 -0800

    added log file feature

--- SampleListGenerator.sh
+++ SampleListGenerator.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #SampleListGenerator.sh input output
 printf '' > $2
+printf '' > SampleListGenerator.log
 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/ -q -O - > index.html
 while read line
     do
         printf $line":"
+        printf $line":" >> SampleListGenerator.log
         if [ $(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | wc -l) != 0 ]
             then
                 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep 'libns:flowcell" resource="' | cut -d/ -f3 > Flowcell
                 while read Flow
                     do
                         printf $Flow
+                        printf $Flow >> SampleListGenerator.log
                         declare -i FlowN=0
                         folder=$(grep $Flow index.html | grep -v $Flow"_temp" | cut -d"\"" -f8)
                         for label in "Unaligned/" "Unaligned.dualIndex/" "Unaligned.singleIndex/"
@@ -20,19 +23,23 @@ while read line
                                         project=$(wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/runfolders/volvox/$folder$label -q -O - | grep $line | cut -d"\"" -f8)
                                         printf https://jumpgate.caltech.edu/runfolders/volvox/$folder$label$project"Sample_"$line"/ " >> $2
                                         printf " got in "$label
+                                        printf " got in "$label >> SampleListGenerator.log
                                         FlowN=$FlowN+1
                                 fi
                             done
                         if [ $FlowN == 0 ]
                             then
                                 printf " Not found"
+                                printf " Not found" >> SampleListGenerator.log
                         fi
                         printf "\n"
+                        printf "\n" >> SampleListGenerator.log
 
                     done<Flowcell
                 wget --user=gec --password=gecilluminadata --no-check-certificate https://jumpgate.caltech.edu/library/$line/ -q -O - | grep libns:name | cut -d"<" -f2 | cut -d">" -f2 >> $2
         else
             printf " no Flowcells found\n"
+            printf " no Flowcells found\n" >> SampleListGenerator.log
         fi
     done <$1
 rm Flowcell
