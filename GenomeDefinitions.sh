87a1f647053e98b40b80e2fecb065e36bfaa0cab
brianpenghe <brianpenghe@gmail.com>
2015-11-16 18:04:57 -0800

    added hg38 blacklist regions

--- GenomeDefinitions.sh
+++ GenomeDefinitions.sh
@@ -38,8 +38,8 @@ then
     fa="/woldlab/castor/proj/genome/bowtie-indexes/hg38.fa"
     bowtieindex="/woldlab/castor/proj/genome/bowtie-indexes/hg38"
     chromsizes="/woldlab/castor/home/phe/genomes/hg38.chrom.sizes"
-    blacklist="/woldlab/castor/home/phe/genomes/hg19/blacklist/wgEncodeDacMapabilityConsensusExcludable.bed"
-    mitoblack="/woldlab/castor/home/phe/genomes/hg19/blacklist/blacklist_.hg19male.30merlS50000mD50s150fL0.bed"
+    blacklist="/woldlab/castor/home/phe/genomes/hg38/hg19blacklist_liftedovertohg38.bed"
+    mitoblack="/woldlab/castor/home/phe/genomes/hg38/chrM30merschrM30mers.hg38.30merlS50000mD50s150fL0.bed"
 fi
 
 
