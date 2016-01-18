95b4f7740f0117afd9d3524e150df80d86403841
brianpenghe <brianpenghe@gmail.com>
2016-01-17 22:55:31 -0800

    removed trimming, fixed tophat bug

--- TophatCodeGenerator.sh
+++ TophatCodeGenerator.sh
@@ -19,10 +19,11 @@ tophatdate=$(date +"%y%m%d")
 printf '''
 universe=vanilla
 
-environment="PATH=$ENV(PATH):/woldlab/castor/home/georgi/programs/bowtie-0.12.9/:/woldlab/castor/home/georgi/programs/samtools-0.1.18:/woldlab/castor/home/georgi/programs/tophat-2.0.8.Linux_x86_64/"
+environment = "PATH=/bin:/usr/bin:/usr/local/bin:/woldlab/castor/proj/genome/programs/bowtie-0.12.9:/woldlab/castor/proj/genome/programs/tophat-2.0.13.Linux_x86_64"
+
 getenv = true
 
-executable=/bin/sh
+executable=/usr/bin/python
 
 log=tophat2.$(Process).log
 output=tophat2.$(Process).out
@@ -34,10 +35,11 @@ request_disk = 0
 
 Requirements = (Machine == "pongo.cacr.caltech.edu" || Machine == "myogenin.cacr.caltech.edu" || Machine == "mondom.cacr.caltech.edu" || Machine == "trog.caltech.edu" || Machine == "wold-clst-3.woldlab" || Machine == "wold-clst-4.woldlab" || Machine == "myostatin.cacr.caltech.edu")
 
-''' >> bowtie$bowtiedate".condor"
+''' >> tophat$tophatdate".condor"
+
 
 while read path
     do
-        printf "arguments=\"-c \' python /woldlab/castor/home/georgi/code/trimfastq.py "$path"allfastq "$3" -stdout | python /woldlab/castor/home/georgi/programs/tophat-2.0.8.Linux_x86_64/tophat -p 4  --GTF /woldlab/castor/home/georgi/genomes/mm9/Mus_musculus.NCBIM37.67.filtered.gtf --transcriptome-index /woldlab/castor/home/georgi/genomes/mm9/bowtie-indexes/transcriptome-indexes/Mus_musculus.NCBIM37.67.filtered  -o "$path"."$2"."$3"mer /woldlab/castor/home/georgi/genomes/mm9/bowtie-indexes/mm9-single-cell-NIST-fixed-spikes - \' \"\nqueue\n" >> tophat$tophatdate".condor"
+        printf "arguments=/woldlab/castor/home/georgi/programs/tophat-2.0.8.Linux_x86_64/tophat --bowtie1 -p 4 --GTF  /woldlab/castor/proj/genome/transcriptome-indexes/Mus_musculus.NCBIM37.67.filtered.gtf --transcriptome-index  /woldlab/castor/proj/genome/transcriptome-indexes/Mus_musculus.NCBIM37.67.filtered -o "$path"."$2"."$3"mer /woldlab/castor/home/georgi/genomes/mm9/bowtie-indexes/mm9-single-cell-NIST-fixed-spikes /woldlab/castor/home/georgi/code/trimfastq.py "$path"allfastq \nqueue\n" >> tophat$tophatdate".condor"
     done <$1
 
