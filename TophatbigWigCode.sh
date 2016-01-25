c9aa94ea8ac979ed5c152c2a320f32d7e4a29ef8
brianpenghe <brianpenghe@gmail.com>
2016-01-24 23:36:20 -0800

    modified & \n for TophatbigWigCode

--- TophatbigWigCode.sh
+++ TophatbigWigCode.sh
@@ -22,7 +22,7 @@ while read line
         printf "condor_run \"/woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools idxstats "$line"."$2"."$3"/accepted_hits.bam > "$line"."$2"."$3"/idxstats\" && " >> testcodebigWig
 
         printf "condor_run \"python /woldlab/castor/home/georgi/code/makewigglefromBAM-NH.py --- "$line"."$2"."$3"/accepted_hits.bam "$chromsizes" "$line"."$2"."$3".tophat.wig -notitle -uniqueBAM -RPM\" && " >> testcodebigWig
-        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".tophat.wig "$chromsizes" "$line"."$2"."$3".tophat.bigWig\" && " >> testcodebigWig
+        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".tophat.wig "$chromsizes" "$line"."$2"."$3".tophat.bigWig\" & \n" >> testcodebigWig
     done <$1
 
 
