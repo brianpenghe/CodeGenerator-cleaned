ec8713617c6ff4dd968ed385cc180d89c9b4200c
brianpenghe <brianpenghe@gmail.com>
2016-01-26 16:26:07 -0800

    added a cleanup.sh for script testing

---
+++ Cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Run these codes in the current SERVER directory
+
+if [ "$1" == "RNAseq" ]
+    then
+        rm testcode* testFolderPath testSampleList tophat*.condor index.html Cufflinks*s SampleListGenerator.log tophat2.*.*
+elif [ "$1" == "ATAC" ]
+    then
+        rm testcode* testFolderPath testSampleList bowtie*.condor index.html SampleListGenerator.log shell.*.*
+else exit "error in project type" 
+fi
+
+
+
