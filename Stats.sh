1b763287df148c3c3ec836f1bb90b194b13ccca2
brianpenghe <brianpenghe@gmail.com>
2016-02-01 15:13:05 -0800

    reformatted echo, added whole.bed peak numbers

--- Stats.sh
+++ Stats.sh
@@ -37,7 +37,7 @@ elif [ "$2" == "hg19male" -o "$2" == "hg38" ]
 elif [ "$2" == "hg19female" ]
     then
         printf '
-            echo "file total_complexity nucleus_complexity eRange3x2Peaks eRange5x4Peaks F-seqPeaks HOMERpeaks processed unique failed suppressed chr1_reads chr2_reads chr3_reads chr4_reads chr5_reads chr6_reads chr7_reads chr8_reads chr9_reads chr10_reads chr11_reads chr12_reads chr13_reads chr14_reads chr15_reads chr16_reads chr17_reads chr18_reads chr19_reads chr20_reads chr21_reads chr22_reads chrX_reads chrM_reads " >> stats
+            echo "file total_complexity nucleus_complexity eRange3x2Peaks eRange5x4Peaks F-seqPeaksAll F-seqPeaks F-seqFRiP HOMERpeaksAll HOMERpeaks HOMERFRiP processed unique failed suppressed chr1_reads chr2_reads chr3_reads chr4_reads chr5_reads chr6_reads chr7_reads chr8_reads chr9_reads chr10_reads chr11_reads chr12_reads chr13_reads chr14_reads chr15_reads chr16_reads chr17_reads chr18_reads chr19_reads chr20_reads chr21_reads chr22_reads chrX_reads chrM_reads " >> stats
         ' >> testcode
 else exit "error in genome version"
 fi
@@ -49,7 +49,19 @@ printf '
 declare -i k=0
 while read line
     do
-        echo -n $line.'$2'.'$3' $(if [ -e $line.'$2'.'$3'mer.SAMstats ]; then cat $line.'$2'.'$3'mer.SAMstats | grep Complexity | cut -f2; else echo 0; fi) $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.SAMstats ]; then cat $line.'$2'.'$3'mer.unique.nochrM.SAMstats | grep Complexity | cut -f2; else echo 0; fi) $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.3x.2RPM.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.3x.2RPM.bed | cut -d" " -f1; else echo 0; fi) $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.5x.4RPM.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.5x.4RPM.bed | cut -d" " -f1; else echo 0; fi) $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.Fseq.v.f0.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.Fseq.v.f0.bed | cut -d" " -f1; else echo 0; fi) $(if [ -e $line.'$2'.'$3'merlS50000mD50s150fL0.bed ]; then wc -l $line.'$2'.'$3'merlS50000mD50s150fL0.bed | cut -d" " -f1; else echo 0; fi) $(cat shell.$k.err | grep processed - | cut -d: -f2) $(cat shell.$k.err | grep least - | cut -d: -f2 | cut -d"(" -f1 ) $(cat shell.$k.err | grep failed - | cut -d: -f2 | cut -d"(" -f1 ) $(cat shell.$k.err | grep suppressed - | cut -d: -f2 | cut -d"(" -f1 ) " " >> stats
+        echo -n $line.'$2'.'$3' \
+        $(if [ -e $line.'$2'.'$3'mer.SAMstats ]; then cat $line.'$2'.'$3'mer.SAMstats | grep Complexity | cut -f2; else echo 0; fi) \
+        $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.SAMstats ]; then cat $line.'$2'.'$3'mer.unique.nochrM.SAMstats | grep Complexity | cut -f2; else echo 0; fi) \
+        $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.3x.2RPM.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.3x.2RPM.bed | cut -d" " -f1; else echo 0; fi) \
+        $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.5x.4RPM.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.5x.4RPM.bed | cut -d" " -f1; else echo 0; fi) \
+        $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.Fseq.v.f0.whole.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.Fseq.v.f0.whole.bed | cut -d" " -f1; else echo 0; fi) \
+        $(if [ -e $line.'$2'.'$3'mer.unique.nochrM.Fseq.v.f0.bed ]; then wc -l $line.'$2'.'$3'mer.unique.nochrM.Fseq.v.f0.bed | cut -d" " -f1; else echo 0; fi) \
+        $(if [ -e $line.'$2'.'$3'merlS50000mD50s150fL0.whole.bed ]; then wc -l $line.'$2'.'$3'merlS50000mD50s150fL0.whole.bed | cut -d" " -f1; else echo 0; fi) \        
+        $(if [ -e $line.'$2'.'$3'merlS50000mD50s150fL0.bed ]; then wc -l $line.'$2'.'$3'merlS50000mD50s150fL0.bed | cut -d" " -f1; else echo 0; fi) \
+        $(cat shell.$k.err | grep processed - | cut -d: -f2) \
+        $(cat shell.$k.err | grep least - | cut -d: -f2 | cut -d"(" -f1 ) \
+        $(cat shell.$k.err | grep failed - | cut -d: -f2 | cut -d"(" -f1 ) \
+        $(cat shell.$k.err | grep suppressed - | cut -d: -f2 | cut -d"(" -f1 ) " " >> stats
 ' >> testcode
 
 if [ "$2" == "mm9" ]
