db2981605cae22cc1a5487985d4f29560381cf8f
brianpenghe <brianpenghe@gmail.com>
2016-02-01 00:08:07 -0800

    keep a non-blacklist version (whole.bed)

--- FseqCode.sh
+++ FseqCode.sh
@@ -19,8 +19,10 @@ while read line
         printf "condor_run \" /woldlab/castor/proj/programs/BEDTools-Version-2.10.1/bin/bamToBed -i "$line"."$2"."$3".unique.nochrM.bam > "$line"."$2"."$3".unique.nochrM.bed \" && " >> testcodeFseq
         printf "mkdir "$line"."$2"."$3"FseqResults/ && " >> testcodeFseq
         printf "condor_run \" /woldlab/castor/proj/programs/fseq-1.84/bin/fseq -v -of bed -f 0 -o "$line"."$2"."$3"FseqResults/ "$line"."$2"."$3".unique.nochrM.bed \" && " >> testcodeFseq
-        printf "cat "$line"."$2"."$3"FseqResults/*.bed | awk '{print \$1\"\\\t\"\$2\"\\\t\"\$3\"\\\t\"\$4\"\\\t225\\\t+\"}' - | grep -v chrM - | sort -k 1d,1 -k 2n,2 | intersectBed -a - -b "$mitoblack" -v | intersectBed -a - -b "$blacklist" -v > "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.bed && " >> testcodeFseq
-        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.bigBed & \n" >> testcodeFseq
+        printf "cat "$line"."$2"."$3"FseqResults/*.bed | awk '{print \$1\"\\\t\"\$2\"\\\t\"\$3\"\\\t\"\$4\"\\\t225\\\t+\"}' - > "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.whole.bed && " >> testcodeFseq
+        printf "grep -v chrM "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.whole.bed | sort -k 1d,1 -k 2n,2 | intersectBed -a - -b "$mitoblack" -v | intersectBed -a - -b "$blacklist" -v > "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.bed && " >> testcodeFseq
+        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.bigBed && " >> testcodeFseq
+        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.whole.bed "$chromsizes" "$line"."$2"."$3".unique.nochrM.Fseq.v.f0.whole.bigBed & \n" >> testcodeFseq
     done <$1
 
 
