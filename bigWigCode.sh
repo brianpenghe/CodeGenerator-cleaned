4cbe6bb726bbcc3a96337f74cb71b872bd1a1069
brianpenghe <brianpenghe@gmail.com>
2016-02-02 17:10:38 -0800

    deleted make5prime feature for bigWig

--- bigWigCode.sh
+++ bigWigCode.sh
@@ -23,15 +23,9 @@ while read line
         printf "condor_run \"python /woldlab/castor/home/hamrhein/bin/bamToBedGraph.py --match --strand + --unique --splice --chromonly --normalize --verbose "$line"."$2"."$3".unique.bam "$line"."$2"."$3".unique.plus.bg4\" && " >> testcodebigWig
         printf "condor_run \"python /woldlab/castor/home/hamrhein/bin/bamToBedGraph.py --match --strand - --unique --splice --chromonly --normalize --verbose "$line"."$2"."$3".unique.bam "$line"."$2"."$3".unique.minus.bg4\" && " >> testcodebigWig
         printf "condor_run \"python /woldlab/castor/home/hamrhein/bin/bamToBedGraph.py --match --unique --splice --chromonly --normalize --verbose "$line"."$2"."$3".unique.bam "$line"."$2"."$3".unique.bg4\" && " >> testcodebigWig
-        printf "condor_run \"python /woldlab/castor/home/georgi/code/make5primeWigglefromBAM-NH.py --- "$line"."$2"."$3".unique.bam  "$chromsizes" "$line"."$2"."$3".unique.5prime.wig -notitle -uniqueBAM -RPM\" && " >> testcodebigWig
-        printf "condor_run \"python /woldlab/castor/home/georgi/code/make5primeWigglefromBAM-NH.py --- "$line"."$2"."$3".unique.bam  "$chromsizes" "$line"."$2"."$3".unique.5prime.plus.wig -stranded + -notitle -uniqueBAM -RPM\" && " >> testcodebigWig
-        printf "condor_run \"python /woldlab/castor/home/georgi/code/make5primeWigglefromBAM-NH.py --- "$line"."$2"."$3".unique.bam  "$chromsizes" "$line"."$2"."$3".unique.5prime.minus.wig -stranded - -notitle -uniqueBAM -RPM\" && " >> testcodebigWig
         printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.plus.bg4 "$chromsizes" "$line"."$2"."$3".unique.plus.bigWig\" && " >> testcodebigWig
         printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.minus.bg4 "$chromsizes" "$line"."$2"."$3".unique.minus.bigWig\" && " >> testcodebigWig
         printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.bg4 "$chromsizes" "$line"."$2"."$3".unique.bigWig\" && " >> testcodebigWig
-        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.5prime.plus.wig "$chromsizes" "$line"."$2"."$3".unique.5prime.plus.bigWig\" && " >> testcodebigWig
-        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.5prime.minus.wig "$chromsizes" "$line"."$2"."$3".unique.5prime.minus.bigWig\" && " >> testcodebigWig
-        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.5prime.wig "$chromsizes" "$line"."$2"."$3".unique.5prime.bigWig\" & \n" >> testcodebigWig
     done <$1
 
 
