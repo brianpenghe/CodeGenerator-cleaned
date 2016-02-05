73e5b83f97fb83e7510d11fa87e4f62fc1fb8540
brianpenghe <brianpenghe@gmail.com>
2016-02-04 22:10:09 -0800

    replaced Henry’s script with Georgi’s so that Sea Urchin Chromosomes can work.

--- bigWigCode.sh
+++ bigWigCode.sh
@@ -20,11 +20,7 @@ while read line
         printf "condor_run \" /woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools index "$line"."$2"."$3".unique.bam \" && " >> testcodebigWig
         printf "condor_run \"python /woldlab/castor/home/georgi/code/SAMstats.py "$line"."$2"."$3".unique.bam "$line"."$2"."$3".SAMstats -bam "$chromsizes" /woldlab/castor/proj/programs/samtools-0.1.8/samtools \" && " >> testcodebigWig
         printf "condor_run \"/woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools idxstats "$line"."$2"."$3".unique.bam > "$line"."$2"."$3".idxstats\" && " >> testcodebigWig
-        printf "condor_run \"python /woldlab/castor/home/hamrhein/bin/bamToBedGraph.py --match --strand + --unique --splice --chromonly --normalize --verbose "$line"."$2"."$3".unique.bam "$line"."$2"."$3".unique.plus.bg4\" && " >> testcodebigWig
-        printf "condor_run \"python /woldlab/castor/home/hamrhein/bin/bamToBedGraph.py --match --strand - --unique --splice --chromonly --normalize --verbose "$line"."$2"."$3".unique.bam "$line"."$2"."$3".unique.minus.bg4\" && " >> testcodebigWig
-        printf "condor_run \"python /woldlab/castor/home/hamrhein/bin/bamToBedGraph.py --match --unique --splice --chromonly --normalize --verbose "$line"."$2"."$3".unique.bam "$line"."$2"."$3".unique.bg4\" && " >> testcodebigWig
-        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.plus.bg4 "$chromsizes" "$line"."$2"."$3".unique.plus.bigWig\" && " >> testcodebigWig
-        printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.minus.bg4 "$chromsizes" "$line"."$2"."$3".unique.minus.bigWig\" && " >> testcodebigWig
+        printf "condor_run \"python /woldlab/castor/home/georgi/code/makewigglefromBAM-NH.py --- "$line"."$2"."$3".unique.bam "$chromsizes" "$line"."$2"."$3".unique.bg4 -notitle -uniqueBAM -RPM\" && " >> testcodebigWig
         printf "condor_run \"/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig "$line"."$2"."$3".unique.bg4 "$chromsizes" "$line"."$2"."$3".unique.bigWig\" && " >> testcodebigWig
     done <$1
 
