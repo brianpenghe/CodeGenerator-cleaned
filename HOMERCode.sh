c7b4293dcdb14fe97be97a1f1a3d28645fb3600b
brianpenghe <brianpenghe@gmail.com>
2016-02-09 22:53:26 -0800

    fixed & bug

--- HOMERCode.sh
+++ HOMERCode.sh
@@ -23,7 +23,7 @@ while read line
         printf "condor_run \"/woldlab/castor/proj/programs/homer-4.7/bin/findPeaks "$line"."$2"."$3"HomerTags -localSize 50000 -minDist 50 -size 150 -fragLength 0 -o "$line"."$2"."$3"lS50000mD50s150fL0 2> "$line"."$2"."$3"lS50000mD50s150fL0.err \" && " >> testcodeHOMER
         printf "grep 000 "$line"."$2"."$3"lS50000mD50s150fL0 | grep chr - | grep -v = | awk '{print \$2\"\\\t\"\$3\"\\\t\"\$4\"\\\t\"\$1\"\\\t225\\\t\"\$5}' - | sort -k 1d,1 -k 2n,2 > "$line"."$2"."$3"lS50000mD50s150fL0.whole.bed && " >> testcodeFseq
         printf "intersectBed -a "$line"."$2"."$3"lS50000mD50s150fL0.whole.bed -b "$mitoblack" -v | intersectBed -a - -b "$blacklist" -v > "$line"."$2"."$3"lS50000mD50s150fL0.bed && " >> testcodeHOMER
-        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3"lS50000mD50s150fL0.bed "$chromsizes" "$line"."$2"."$3"lS50000mD50s150fL0.bigBed & \n" >> testcodeHOMER
+        printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3"lS50000mD50s150fL0.bed "$chromsizes" "$line"."$2"."$3"lS50000mD50s150fL0.bigBed && " >> testcodeHOMER
         printf "/woldlab/castor/proj/programs/x86_64/bedToBigBed "$line"."$2"."$3"lS50000mD50s150fL0.whole.bed "$chromsizes" "$line"."$2"."$3"lS50000mD50s150fL0.whole.bigBed & \n" >> testcodeHOMER
     done <$1
 
