e6efbb58f080bfdb6d4679bca3853b772cea376c
brianpenghe <brianpenghe@gmail.com>
2016-02-18 12:29:40 -0800

    separated Stats code into a runnable script

--- Stats.sh
+++ Stats.sh
@@ -5,16 +5,14 @@
 
 #usage: ~/programs/Stats.sh testFolderPath mm9 30mer
 
-echo '' >> testcode
-echo '' >> testcode
+echo '' > testcodeStats
 
 CurrentLo=$(pwd)
 source ~/programs/GenomeDefinitions.sh $2
 
-echo '' >> testcode
-echo "******take a break***********" >> testcode
-echo "Summarizing all the scores" >> testcode
-echo "*****************" >> testcode
+echo "#!/bin/bash" >> testcodeStats
+echo "#Summarizing all the scores" >> testcodeStats
+echo "#*****************" >> testcodeStats
 
 
 
@@ -23,22 +21,22 @@ if [ "$2" == "mm9" ]
     then
         printf '
             echo "file total_complexity nucleus_complexity eRange3x2Peaks eRange5x4Peaks F-seqPeaksAll F-seqPeaks F-seqFRiP HOMERpeaksAll HOMERpeaks HOMERFRiP processed unique failed suppressed chr1_reads chr2_reads chr3_reads chr4_reads chr5_reads chr6_reads chr7_reads chr8_reads chr9_reads chr10_reads chr11_reads chr12_reads chr13_reads chr14_reads chr15_reads chr16_reads chr17_reads chr18_reads chr19_reads chrX_reads chrY_reads chrM_reads " >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "mm10" ]
     then
         printf '
             echo "file total_complexity nucleus_complexity eRange3x2Peaks eRange5x4Peaks F-seqPeaksAll F-seqPeaks F-seqFRiP HOMERpeaksAll HOMERpeaks HOMERFRiP processed unique failed suppressed chr1_reads chr2_reads chr3_reads chr4_reads chr5_reads chr6_reads chr7_reads chr8_reads chr9_reads chr10_reads chr11_reads chr12_reads chr13_reads chr14_reads chr15_reads chr16_reads chr17_reads chr18_reads chr19_reads chrX_reads chrY_reads chrM_reads " >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "hg19male" -o "$2" == "hg38" ]
     then
         printf '
             echo "file total_complexity nucleus_complexity eRange3x2Peaks eRange5x4Peaks F-seqPeaksAll F-seqPeaks F-seqFRiP HOMERpeaksAll HOMERpeaks HOMERFRiP processed unique failed suppressed chr1_reads chr2_reads chr3_reads chr4_reads chr5_reads chr6_reads chr7_reads chr8_reads chr9_reads chr10_reads chr11_reads chr12_reads chr13_reads chr14_reads chr15_reads chr16_reads chr17_reads chr18_reads chr19_reads chr20_reads chr21_reads chr22_reads chrX_reads chrY_reads chrM_reads " >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "hg19female" ]
     then
         printf '
             echo "file total_complexity nucleus_complexity eRange3x2Peaks eRange5x4Peaks F-seqPeaksAll F-seqPeaks F-seqFRiP HOMERpeaksAll HOMERpeaks HOMERFRiP processed unique failed suppressed chr1_reads chr2_reads chr3_reads chr4_reads chr5_reads chr6_reads chr7_reads chr8_reads chr9_reads chr10_reads chr11_reads chr12_reads chr13_reads chr14_reads chr15_reads chr16_reads chr17_reads chr18_reads chr19_reads chr20_reads chr21_reads chr22_reads chrX_reads chrM_reads " >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "strPur2" -o "$2" == "galGal4" ]
     then
         printf "skip stats.sh"
@@ -68,44 +66,44 @@ while read line
         $(cat shell.$k.err | grep least - | cut -d: -f2 | cut -d"(" -f1 ) \
         $(cat shell.$k.err | grep failed - | cut -d: -f2 | cut -d"(" -f1 ) \
         $(cat shell.$k.err | grep suppressed - | cut -d: -f2 | cut -d"(" -f1 ) " " >> stats
-' >> testcode
+' >> testcodeStats
 
 if [ "$2" == "mm9" ]
     then
         for i in {1..19} 'X' 'Y' 'M'
             do
-                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcode
+                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcodeStats
                     done
         printf '
             echo $chr1_reads $chr2_reads $chr3_reads $chr4_reads $chr5_reads $chr6_reads $chr7_reads $chr8_reads $chr9_reads $chr10_reads $chr11_reads $chr12_reads $chr13_reads $chr14_reads $chr15_reads $chr16_reads $chr17_reads $chr18_reads $chr19_reads $chrX_reads $chrY_reads $chrM_reads >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "mm10" ]
     then
         for i in {1..19} 'X' 'Y' 'M'
             do
-                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcode
+                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcodeStats
             done
         printf '
             echo $chr1_reads $chr2_reads $chr3_reads $chr4_reads $chr5_reads $chr6_reads $chr7_reads $chr8_reads $chr9_reads $chr10_reads $chr11_reads $chr12_reads $chr13_reads $chr14_reads $chr15_reads $chr16_reads $chr17_reads $chr18_reads $chr19_reads $chrX_reads $chrY_reads $chrM_reads >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "hg19male" -o "$2" == "hg38" ]
     then
         for i in {1..22} 'X' 'Y' 'M'
             do
-                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcode
+                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcodeStats
             done
         printf '
             echo $chr1_reads $chr2_reads $chr3_reads $chr4_reads $chr5_reads $chr6_reads $chr7_reads $chr8_reads $chr9_reads $chr10_reads $chr11_reads $chr12_reads $chr13_reads $chr14_reads $chr15_reads $chr16_reads $chr17_reads $chr18_reads $chr19_reads $chr20_reads $chr21_reads $chr22_reads $chrX_reads $chrY_reads $chrM_reads >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "hg19female" ]
     then
         for i in {1..22} 'X' 'M'
             do
-                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcode
+                echo "        chr"$i"_reads=\$(egrep -w 'chr"$i"' "\$line"."$2"."$3"mer.idxstats | cut -f3 | awk '{sum+=\$1} END {print sum}')" >> testcodeStats
             done
         printf '
             echo $chr1_reads $chr2_reads $chr3_reads $chr4_reads $chr5_reads $chr6_reads $chr7_reads $chr8_reads $chr9_reads $chr10_reads $chr11_reads $chr12_reads $chr13_reads $chr14_reads $chr15_reads $chr16_reads $chr17_reads $chr18_reads $chr19_reads $chr20_reads $chr21_reads $chr22_reads $chrX_reads $chrM_reads >> stats
-        ' >> testcode
+        ' >> testcodeStats
 elif [ "$2" == "strPur2" -o "$2" == "galGal4" ]
     then
         printf "skip stats.sh"
@@ -115,7 +113,7 @@ fi
 printf '
     k=$k+1
     done <'$1'
-' >> testcode
+' >> testcodeStats
 
-
+chmod a+x testcodeStats
 
