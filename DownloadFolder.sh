b8b9161da62b5e8189c8da68247bcdad6032b44d
brianpenghe <brianpenghe@gmail.com>
2016-03-10 14:01:18 -0800

    added ftp feature for SRA data from GEO

--- DownloadFolder.sh
+++ DownloadFolder.sh
@@ -9,10 +9,16 @@ while read line
     do
         declare -i k
         k=1
-        while [[ $(echo $line | cut -d' ' -f$k | cut -c1-4) == "http" ]]
+        while [[ $(echo $line | cut -d' ' -f$k | cut -c1-4) == "http" || $(echo $line | cut -d' ' -f$k | cut -c1-3) == "ftp" ]]
             do
                 Download=$(echo $line | cut -d' ' -f$k | sed "s/https:/http:/g")
-                echo 'wget -r --no-parent --no-check-certificate '$Download' ' >> testcode
+				if [[ $(echo $line | cut -d' ' -f$k | cut -c1-4) == "http" ]]
+					then
+						echo 'wget -r --no-parent --no-check-certificate '$Download' ' >> testcode
+				elif [[ $(echo $line | cut -d' ' -f$k | cut -c1-3) == "ftp" ]]
+					then
+						echo 'wget --no-parent --no-check-certificate '$Download' ' >> testcode
+				fi
                 k=$k+1
             done
     done <$1
