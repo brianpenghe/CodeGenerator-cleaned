8050f6ebcbb2bc3c8dab29e6d003fdea05ba8640
brianpenghe <brianpenghe@gmail.com>
2016-03-10 20:41:56 -0800

    added a script to deal with GEO RNA-seq data

---
+++ GEOCodeGeneratorRNAseq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Run these codes in the current SERVER directory
+#it generates three testfiles: testcode, testFolderPath and testSampleList
+#usage: ~/programs/GECCodeGeneratorRNAseq.sh test mm9 36 PE
+#test file is just a list of library ID(number)s.
+#this script enables combining multiple flowcells.
+
+/woldlab/castor/home/phe/programs/Cleanup.sh RNAseq
+echo '' > testcode
+CurrentLo=$(pwd)
+/woldlab/castor/home/phe/programs/GEOSampleListGenerator.sh $1 testSampleList
+source /woldlab/castor/home/phe/programs/GenomeDefinitions.sh $2
+/woldlab/castor/home/phe/programs/DownloadFolder.sh testSampleList
+source /woldlab/castor/home/phe/programs/GEOrefolder.sh $4 $2 $3
+
+
+/woldlab/castor/home/phe/programs/TophatCodeGenerator.sh testFolderPath $2 $3 $4
+
+/woldlab/castor/home/phe/programs/RNAseQC.sh testFolderPath $2 $3"mer"
+
+/woldlab/castor/home/phe/programs/TophatbigWigCode.sh testFolderPath $2 $3"mer"
+
+/woldlab/castor/home/phe/programs/HTseqCodeGenerator.sh testFolderPath $2 $3 $4
+
+/woldlab/castor/home/phe/programs/CufflinksCode.sh testFolderPath $2 $3"mer"
+
+/woldlab/castor/home/phe/programs/TrackSummary.sh bigWig
+
+/woldlab/castor/home/phe/programs/TophatCufflinksStats.sh testFolderPath $2 $3
+
+
