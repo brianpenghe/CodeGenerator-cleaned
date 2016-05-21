c8cb13aed91bddbd9a1baf9b79664da124d15c12
brianpenghe <brianpenghe@gmail.com>
2016-05-21 12:10:17 -0700

    [bug fixed]now calling tophatCLIP

--- LocalCodeGeneratorCLIPseq.sh
+++ LocalCodeGeneratorCLIPseq.sh
@@ -15,7 +15,7 @@ source /woldlab/castor/home/phe/programs/GenomeDefinitions.sh $2
 
 source /woldlab/castor/home/phe/programs/LocalrefolderCLIP.sh $1 $2 $3 $4
 
-/woldlab/castor/home/phe/programs/TophatCodeGenerator.sh testFolderPath $2 $3 $4
+/woldlab/castor/home/phe/programs/TophatCLIPCodeGenerator.sh testFolderPath $2 $3 $4
 
 /woldlab/castor/home/phe/programs/RNAseQC.sh testFolderPath $2 $3"mer"
 
