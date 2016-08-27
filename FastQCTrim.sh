0919c935a88fb205e7faf59137b1292684547efc
brianpenghe <brianpenghe@gmail.com>
2016-08-26 17:27:48 -0700

    now cut adapt before trimmomatic

--- FastQCTrim.sh
+++ FastQCTrim.sh
@@ -9,9 +9,9 @@ if [[ "$1" == "PE" ]]
         printf "mkdir "$2"FastQCk6R2 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R1allfastq -o "$2"FastQCk6R1 -k 6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2allfastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
-        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -trimlog "$2"trim.log "$2"R1allfastq "$2"R2allfastq "$2"R1trimmedfastq "$2"R1allunpairedfastq "$2"R2trimmedfastq "$2"R2allunpairedfastq ILLUMINACLIP:NexteraAdaptors.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:"$(($3==0?20:$3))" && " >> testcode
-        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R1trimmedfastq > "$2"R1allpairedfastq 2> "$2"R1allpairedfastq.log && "
-        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R2trimmedfastq > "$2"R2allpairedfastq 2> "$2"alltrimmedfastq.log && "
+        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R1allfastq > "$2"R1trimmedfastq 2> "$2"R1trimmedfastq.log && "
+        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R2allfastq > "$2"R2trimmedfastq 2> "$2"R2trimmedfastq.log && "
+        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -trimlog "$2"trimmomatic.log "$2"R1trimmedfastq "$2"R2trimmedfastq "$2"R1allpairedfastq "$2"R1allunpairedfastq "$2"R2allpairedfastq "$2"R2allunpairedfastq ILLUMINACLIP:NexteraAdaptors.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:"$(($3==0?20:$3))" && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R1allpairedfastq -o "$2"FastQCk6R1 -k 6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2allpairedfastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
         if [[ "$3" == "0" ]]
@@ -27,8 +27,8 @@ elif [[ "$1" == "SE" ]]
         printf "cat "$2"*.fastq > "$2"allfastq && " >> testcode
         printf "mkdir "$2"FastQCk6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"allfastq -o "$2"FastQCk6 -k 6 && " >> testcode
-        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trim.log "$2"allfastq "$2"trimmedfastq ILLUMINACLIP:NexteraAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
-        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"trimmedfastq > "$2"alltrimmedfastq 2> "$2"alltrimmedfastq.log && "
+        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"allfastq > "$2"trimmedfastq 2> "$2"trimmedfastq.log && "
+        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trimmomatic.log "$2"trimmedfastq "$2"alltrimmedfastq ILLUMINACLIP:NexteraAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"alltrimmedfastq -o "$2"FastQCk6 -k 6 && " >> testcode
         if [[ "$3" == "0" ]]
             then
