c32e349997af6fac2174913b10fa4ba3a5fd8e2d
brianpenghe <brianpenghe@gmail.com>
2016-09-01 23:10:46 -0700

    fixed important syntax bug

--- FastQCTrim.sh
+++ FastQCTrim.sh
@@ -11,7 +11,7 @@ if [[ "$1" == "PE" ]]
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2all.fastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
         printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R1all.fastq > "$2"R1trimmedfastq 2> "$2"R1trimmedfastqcutadapt.log && " >> testcode
         printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R2all.fastq > "$2"R2trimmedfastq 2> "$2"R2trimmedfastqcutadapt.log && " >> testcode
-        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -trimlog "$2"trimmomatic.log "$2"R1trimmedfastq "$2"R2trimmedfastq "$2"R1allpairedfastq "$2"R1allunpairedfastq "$2"R2allpairedfastq "$2"R2allunpairedfastq ILLUMINACLIP:~/genomes/NexteraAdaptors.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:"$(($3==0?20:$3))" && " >> testcode
+        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -trimlog "$2"trimmomatic.log "$2"R1trimmedfastq "$2"R2trimmedfastq "$2"R1allpairedfastq "$2"R1allunpairedfastq "$2"R2allpairedfastq "$2"R2allunpairedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/NexteraAdaptors.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:"$(($3==0?20:$3))" && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R1allpairedfastq -o "$2"FastQCk6R1 -k 6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2allpairedfastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
         if [[ "$3" == "0" ]]
@@ -28,7 +28,7 @@ elif [[ "$1" == "SE" ]]
         printf "mkdir "$2"FastQCk6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"all.fastq -o "$2"FastQCk6 -k 6 && " >> testcode
         printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"all.fastq > "$2"trimmedfastq 2> "$2"trimmedfastqcutadapt.log && " >> testcode
-        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trimmomatic.log "$2"trimmedfastq "$2"alltrimmedfastq ILLUMINACLIP:~/genomes/NexteraAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
+        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trimmomatic.log "$2"trimmedfastq "$2"alltrimmedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/NexteraAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"alltrimmedfastq -o "$2"FastQCk6 -k 6 && " >> testcode
         if [[ "$3" == "0" ]]
             then
