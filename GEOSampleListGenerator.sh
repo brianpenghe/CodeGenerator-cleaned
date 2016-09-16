c965784ab2f95dd33d9de4bc9013d84cff7becd7
brianpenghe@gmail.com <brianpenghe@gmail.com>
2016-09-16 14:46:10 -0700

    [IMPORTANT]fixed NCBI blocking issue

--- GEOSampleListGenerator.sh
+++ GEOSampleListGenerator.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SampleListGenerator.sh input output
-printf '' > $2e
+printf '' > $2
 printf '' > SampleListGenerator.log
 while read line
     do
         printf $line":"
         printf $line":" >> SampleListGenerator.log
-        if [ $(wget --no-check-certificate https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$line -q -O - | grep 'ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra' | wc -l) != 0 ]
+        if [ $(sleep 1s; wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$line -q -O - | grep 'ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra' | wc -l) != 0 ]
             then
-				metadata=$(wget --no-check-certificate https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$line -q -O - | grep -A 1 Title | grep justify | cut -d\> -f2 | cut -d\< -f1 | sed -r "s/[/\ #;&~]/_/g")
-                SRXlink=$(wget --no-check-certificate https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$line -q -O - | grep 'ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra' | cut -d\" -f4)
-				wget $SRXlink/ -q -O - | grep SRR | cut -d\" -f2 > SRRlinks
+				metadata=$(sleep 1s; wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$line -q -O - | grep -A 1 Title | grep justify | cut -d\> -f2 | cut -d\< -f1 | sed -r "s/[/\ #;&~]/_/g")
+                SRXlink=$(sleep 1s; wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$line -q -O - | grep 'ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra' | cut -d\" -f4)
+				sleep 1s; wget --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" $SRXlink/ -q -O - | grep SRR | cut -d\" -f2 > SRRlinks
                 while read SRRlink
                     do
-                        SRAlink=$(wget $SRRlink/ -q -O - | grep .sra\" | cut -d\" -f2)
+                        SRAlink=$(sleep 1s; wget --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" $SRRlink/ -q -O - | grep .sra\" | cut -d\" -f2)
                         printf $SRAlink" " >> $2
                         printf " found "$(echo $SRAlink | rev | cut -d/ -f1 | rev )" "
                         printf " found "$(echo $SRAlink | rev | cut -d/ -f1 | rev )" " >> SampleListGenerator.log
@@ -27,4 +27,3 @@ while read line
             printf " no sra found " >> SampleListGenerator.log
         fi
     done <$1
-
