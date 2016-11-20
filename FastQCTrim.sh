11a582cb02b0db7acee5aceaefc6cd4d1ef853e5
brianpenghe@gmail.com <brianpenghe@gmail.com>
2016-11-20 14:59:26 -0800

    appended adapter list

--- FastQCTrim.sh
+++ FastQCTrim.sh
@@ -9,9 +9,9 @@ if [[ "$1" == "PE" ]]
         printf "mkdir "$2"FastQCk6R2 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R1all.fastq -o "$2"FastQCk6R1 -k 6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2all.fastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
-        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R1all.fastq > "$2"R1trimmedfastq 2> "$2"R1trimmedfastqcutadapt.log && " >> testcode
-        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R2all.fastq > "$2"R2trimmedfastq 2> "$2"R2trimmedfastqcutadapt.log && " >> testcode
-        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -trimlog "$2"trimmomatic.log "$2"R1trimmedfastq "$2"R2trimmedfastq "$2"R1allpairedfastq "$2"R1allunpairedfastq "$2"R2allpairedfastq "$2"R2allunpairedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/NexteraAdaptors.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:"$(($3==0?20:$3))" && " >> testcode
+        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R1all.fastq | ~/.local/bin/cutadapt -g TGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g CTTTGTGTTTGA - > "$2"R1trimmedfastq && " >> testcode
+        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R2all.fastq | ~/.local/bin/cutadapt -g TGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g CTTTGTGTTTGA - > "$2"R2trimmedfastq && " >> testcode
+        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar PE -threads 4 -trimlog "$2"trimmomatic.log "$2"R1trimmedfastq "$2"R2trimmedfastq "$2"R1allpairedfastq "$2"R1allunpairedfastq "$2"R2allpairedfastq "$2"R2allunpairedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/AllAdaptors.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:"$(($3==0?20:$3))" && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R1allpairedfastq -o "$2"FastQCk6R1 -k 6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2allpairedfastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
         if [[ "$3" == "0" ]]
@@ -27,8 +27,8 @@ elif [[ "$1" == "SE" ]]
         printf "cat "$2"*.fastq > "$2"all.fastq && " >> testcode
         printf "mkdir "$2"FastQCk6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"all.fastq -o "$2"FastQCk6 -k 6 && " >> testcode
-        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"all.fastq > "$2"trimmedfastq 2> "$2"trimmedfastqcutadapt.log && " >> testcode
-        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trimmomatic.log "$2"trimmedfastq "$2"alltrimmedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/NexteraAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
+        printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"all.fastq | ~/.local/bin/cutadapt -g TGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g CTTTGTGTTTGA - > "$2"trimmedfastq && " >> testcode
+        printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trimmomatic.log "$2"trimmedfastq "$2"alltrimmedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/AllAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"alltrimmedfastq -o "$2"FastQCk6 -k 6 && " >> testcode
         if [[ "$3" == "0" ]]
             then
