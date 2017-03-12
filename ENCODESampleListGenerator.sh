77ed82f463371e647621fc89ead034978df64c9e
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-03-11 22:33:24 -0800

    fixed bugs and added progress info

--- ENCODESampleListGenerator.sh
+++ ENCODESampleListGenerator.sh
@@ -5,6 +5,7 @@ wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x6
 
 while read line
     do
-        wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" --user=BUFXRY6P --password=hv7oczhoda2i6ygz https://www.encodeproject.org$line -q -O - | tr , '\n' | grep fastq.gz | grep -v submitted_file_name | rev | cut -d\" -f2 | rev | awk '{print "https://www.encodeproject.org"$1}'> ENCODE.fastqs
-        wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" --user=BUFXRY6P --password=hv7oczhoda2i6ygz https://www.encodeproject.org$line -q -O - | tr , '\n' | tr '\<' '\n' | grep "\"summary\":" | rev | cut -d\" -f2 | rev | sed -r "s/[/\ #;&~()]/_/g" > ENCODE.meta
+        echo $line
+        wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" --user=BUFXRY6P --password=hv7oczhoda2i6ygz https://www.encodeproject.org$line -q -O - | tr , '\n' | grep fastq.gz | grep -v submitted_file_name | rev | cut -d\" -f2 | rev | awk '{print "https://www.encodeproject.org"$1}'>> ENCODE.fastqs
+        wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" --user=BUFXRY6P --password=hv7oczhoda2i6ygz https://www.encodeproject.org$line -q -O - | tr , '\n' | tr '\<' '\n' | grep "\"summary\":" | rev | cut -d\" -f2 | rev | sed -r "s/[/\ #;&~()]/_/g" >> ENCODE.meta
     done <ENCODE.experiments
