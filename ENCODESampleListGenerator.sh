f5527a311c0193b43660384f6cb3e7d725ca9dd9
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-04-24 16:12:09 -0700

    added code for cleaning up files before writing into

--- ENCODESampleListGenerator.sh
+++ ENCODESampleListGenerator.sh
@@ -4,6 +4,9 @@ source /woldlab/castor/home/phe/programs/NovelCharacterDefinition.sh
 
 wget --no-check-certificate --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36" --user=BUFXRY6P --password=hv7oczhoda2i6ygz $1 -q -O - | tr , '\n' | tr '\>' '\n' | grep clearfix | rev | cut -d\$ -f1 | rev | cut -d. -f1 > ENCODE.experiments
 
+echo '' > ENCODE.fastqs
+echo '' > ENCODE.meta
+
 while read line
     do
         echo $line
