5ec267f52569b4e72c2410606c48f9b7b908d2fe
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-12-07 15:18:58 -0800

    added a script to sort bam files, for the sake of condor systems

---
+++ SamtoolsSort.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Bulk sort with default naming method
+#the file $bam is just a list of bam files for sorting
+#usage: ./SamtoolsSort.sh bams
+
+while read bam
+    do
+        /woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools sort $bam $(echo $bam | rev | cut -d. -f2- | rev).sorted &&
+        /woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools index $(echo $bam | rev | cut -d. -f2- | rev).sorted.bam &
+    done<bams
