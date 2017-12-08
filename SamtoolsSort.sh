81eb6f37909c5725c75c3cf0de330ae12d17a493
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-12-08 08:46:13 -0800

    fixed a bug: condor doesn’t like programs which run  codes in the background. So & is not allowed

--- SamtoolsSort.sh
+++ SamtoolsSort.sh
@@ -2,4 +2,4 @@
 #usage: ./SamtoolsSort.sh bamfile
 
 /woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools sort $1 $(echo $1 | rev | cut -d. -f2- | rev).sorted &&
-        /woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools index $(echo $1 | rev | cut -d. -f2- | rev).sorted.bam &
+        /woldlab/castor/proj/programs/samtools-0.1.16/bin/samtools index $(echo $1 | rev | cut -d. -f2- | rev).sorted.bam 
