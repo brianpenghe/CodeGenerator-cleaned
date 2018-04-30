fedc51a05b3d5bd4729fdee3a3f48cdf8489ae76
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-04-30 14:43:09 -0700

    updated program path due to Diane’s failure in sync

--- CoverageBigWig.sh
+++ CoverageBigWig.sh
@@ -6,5 +6,5 @@ uniquewig=$(echo -e $1"Signal.Unique.str1.out.bg")
 allwig=$(echo -e $1"Signal.UniqueMultiple.str1.out.bg")
 
 python /woldlab/castor/home/sau/code/gene_coverage_wig_gtf.py $2 $uniquewig 1000 $1".coverage"
-/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig -clip $uniquewig $3 $uniquewig.bigWig
-/woldlab/castor/proj/genome/programs/x86_64/wigToBigWig -clip $allwig $3 $allwig.bigWig
+/woldlab/castor/proj/programs/x86_64/wigToBigWig -clip $uniquewig $3 $uniquewig.bigWig
+/woldlab/castor/proj/programs/x86_64/wigToBigWig -clip $allwig $3 $allwig.bigWig
