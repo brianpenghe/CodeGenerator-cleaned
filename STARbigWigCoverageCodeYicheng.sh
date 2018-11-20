f69ee74727e9d2b3109eefa83dbd9051c952afee
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-11-20 14:32:03 -0800

    added a script to generate bigWig files for Yicheng’s total RNA-seq

---
+++ STARbigWigCoverageCodeYicheng.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#Run these codes in the current SERVER directory
+#the file $test has two columns, link and name, only one space allowed
+
+#usage: ./STARCodeGenerator.sh testFolderPath mm9 30
+CurrentLo=$(pwd)
+source ~/programs/GenomeDefinitions.sh $2
+
+STARdate=$(date +"%y%m%d")
+
+echo '' > bedgraph$STARdate.condor
+printf '''
+universe=vanilla
+log=bedgraph-star-$(Process).log
+output=bedgraph-star-$(Process).out
+error=bedgraph-star-$(Process).err
+
+STAR_DIR=/woldlab/castor/proj/programs/STAR-2.5.2a/bin/Linux_x86_64/
+
+request_cpus = 1
+request_memory = 4G
+
+executable=$(STAR_DIR)STAR
+transfer_executable=false
+should_transfer_files=IF_NEEDED
+
+should_transfer_files=Always
+when_to_transfer_output=ON_EXIT
+transfer_input_files=/woldlab/loxcyc/home/phe/programs/CoverageBigWig.sh
+
+''' >> bedgraph$STARdate.condor
+
+while read path
+    do
+        echo -e 'initialdir='$path'FastQCk6/Unique/'\
+        '\narguments="--runMode inputAlignmentsFromBAM' \
+        '--inputBAMfile '$path'FastQCk6/Unique/'$2'.'$3'merAligned.sortedByCoord.out.bam ' \
+        '--outWigType bedGraph' \
+        '--outWigStrand Unstranded' \
+        '--outFileNamePrefix '$path'FastQCk6/Unique/'$2'.'$3'merAligned.sortedByCoord' \
+        '--outWigReferencesPrefix chr"' \
+        '\n+PostCmd="CoverageBigWig.sh"\n+PostArguments="'$path'FastQCk6/Unique/'$2'.'$3'merAligned.sortedByCoord '$gtf' '$chromsizes'"\nqueue\n '>> bedgraph$STARdate.condor
+#        echo -e 'initialdir='$path'FastQCk6/Multi/'\
+#        '\narguments="--runMode inputAlignmentsFromBAM' \
+#        '--inputBAMfile '$path'FastQCk6/Multi/'$2'.'$3'merAligned.sortedByCoord.out.bam ' \
+#        '--outWigType bedGraph' \
+#        '--outWigStrand Unstranded' \
+#        '--outFileNamePrefix '$path'FastQCk6/Multi/'$2'.'$3'merAligned.sortedByCoord' \
+#        '--outWigReferencesPrefix chr"' \
+#        '\n+PostCmd="CoverageBigWig.sh"\n+PostArguments="'$path'FastQCk6/Multi/'$2'.'$3'merAligned.sortedByCoord '$gtf' '$chromsizes'"\nqueue\n '>> bedgraph$STARdate.condor
+    done <$1
