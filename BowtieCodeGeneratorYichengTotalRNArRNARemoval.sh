5b83ce0a915af62c1cc94015ac83ebe15175853a
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-11-20 14:31:13 -0800

    added a script to remove ribosome RNAs for Yicheng

---
+++ BowtieCodeGeneratorYichengTotalRNArRNARemoval.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Run these codes in the current SERVER directory
+#the file $test has two columns, link and name, only one space allowed
+
+#usage: ./BowtieCodeGeneratorYichengTotalrRNARemoval.sh testFolderPath dm3 30mer
+
+CurrentLo=$(pwd)
+source ~/programs/GenomeDefinitions.sh $2
+
+bowtiedate=$(date +"%y%m%d")
+echo '' > bowtie$bowtiedate".condor"
+printf '''
+universe=vanilla
+
+executable=/bin/sh
+
+log=shell.$(Process).log
+output=shell.$(Process).out
+error=shell.$(Process).err
+
+request_cpus = 8
+request_memory = 4000
+request_disk = 0
+
+Requirements = (Machine == "pongo.caltech.edu" || Machine == "myogenin.caltech.edu" || Machine == "mondom.caltech.edu" || Machine == "trog.caltech.edu" || Machine == "wold-clst-3.woldlab" || Machine == "wold-clst-4.woldlab" || Machine == "myostatin.caltech.edu")
+
+''' >> bowtie$bowtiedate".condor"
+
+while read line
+    do
+        if [[ "$4" == "PE" ]]
+            then
+                printf "arguments=\"-c \' /woldlab/castor/proj/genome/programs/bowtie-1.0.1+hamrhein_nh_patch/bowtie "$rRNAbowtieindex" -p 8 -v 2 -k 1 --best -X 2000 -t --sam-nh -q -1 "$line"R1allfastq"$3" -2 "$line"R2allfastq"$3" --un "$line"allfastqrRNAUnmapped"$3".fastq "$line"allfastq.rRNA.mapped"$3".map 2> "$line"allfastq.rRNA.mapped"$3".err \' \"\nqueue\n" >> bowtie$bowtiedate".condor"
+        elif [[ "$4" == "SE" ]]
+            then
+                printf "arguments=\"-c \' /woldlab/castor/proj/genome/programs/bowtie-1.0.1+hamrhein_nh_patch/bowtie "$rRNAbowtieindex" -p 8 -v 2 -k 1 --best -t --sam-nh -q "$line"allfastq"$3" --un "$line"allfastqrRNAUnmapped"$3".fastq "$line"allfastq.rRNA.mapped"$3".map 2> "$line"allfastq.rRNA.mapped"$3".err \' \"\nqueue\n" >> bowtie$bowtiedate".condor"
+        fi
+    done <$1
