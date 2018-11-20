d4c3055ff13752a601c8732f45e4b667c36823eb
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-11-20 14:31:00 -0800

    now make subfolders for Yicheng mapping

--- FastQCTrim.sh
+++ FastQCTrim.sh
@@ -8,6 +8,8 @@ if [[ "$1" == "PE" ]]
         printf "mkdir -p "$2"FastQCk6R1 && " >> testcode
         printf "mkdir -p "$2"FastQCk6R2 && " >> testcode
         printf "mkdir -p "$2"FastQCk6 && " >> testcode
+        printf "mkdir -p "$2"FastQCk6/Unique && " >> testcode
+        printf "mkdir -p "$2"FastQCk6/Multi && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R1all.fastq -o "$2"FastQCk6R1 -k 6 && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"R2all.fastq -o "$2"FastQCk6R2 -k 6 && " >> testcode
         printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"R1all.fastq | ~/.local/bin/cutadapt -a CGTATGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g TGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g GGTAACTTTGTGTTT - | ~/.local/bin/cutadapt -g CTTTGTGTTTGA - | ~/.local/bin/cutadapt -a CACTCGTCGGCAGCGTTAGATGTGTATAAG - > "$2"R1trimmedfastq && " >> testcode
@@ -27,6 +29,8 @@ elif [[ "$1" == "SE" ]]
     then
         printf "cat "$2"*.fastq > "$2"all.fastq && " >> testcode
         printf "mkdir -p "$2"FastQCk6 && " >> testcode
+        printf "mkdir -p "$2"FastQCk6/Unique && " >> testcode
+        printf "mkdir -p "$2"FastQCk6/Multi && " >> testcode
         printf "/woldlab/castor/proj/programs/FastQC-0.11.3/fastqc "$2"all.fastq -o "$2"FastQCk6 -k 6 && " >> testcode
         printf "~/.local/bin/cutadapt -a CTGTCTCTTATACAC "$2"all.fastq | ~/.local/bin/cutadapt -a CGTATGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g TGCCGTCTTCTGCTTG - | ~/.local/bin/cutadapt -g GGTAACTTTGTGTTT - | ~/.local/bin/cutadapt -g CTTTGTGTTTGA - | ~/.local/bin/cutadapt -a CACTCGTCGGCAGCGTTAGATGTGTATAAG - > "$2"trimmedfastq && " >> testcode
         printf "java -jar /woldlab/castor/proj/programs/Trimmomatic-0.33/trimmomatic-0.33.jar SE -threads 4 -trimlog "$2"trimmomatic.log "$2"trimmedfastq "$2"alltrimmedfastq ILLUMINACLIP:/woldlab/castor/home/phe/genomes/AllAdaptors.fa:2:30:10 MAXINFO:35:0.9 MINLEN:"$(($3==0?20:$3))" && " >> testcode
