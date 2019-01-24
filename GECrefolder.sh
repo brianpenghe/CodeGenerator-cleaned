fcb9b0c6e34debf7185f2218d7ea35a8d4c95a34
brianpenghe@gmail.com <brianpenghe@gmail.com>
2019-01-23 23:07:31 -0800

    updated comments

--- GECrefolder.sh
+++ GECrefolder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #GECrefolder.sh PE mm9 30
+#GECrefolder.sh PE mm9 30 23 29
 #Thie script also needs a file named testSampleList
 #this file contains links to the data to aggregate and its metadata
 #e.g each line can be http://jumpgate.caltech.edu/runfolders/volvox/141121_SN787_0295_BHBE3UADXX/Unaligned/Project_15160_indexN707-N505/Sample_15160/ http://jumpgate.caltech.edu/runfolders/volvox/141110_SN787_0290_AHCCNNADXX/Unaligned/Project_15160_indexN707-N505/Sample_15160/  Sample15160
