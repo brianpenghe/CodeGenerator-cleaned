27dd4d9c00081770dc73f306f4078acd4935db0b
brianpenghe <brianpenghe@gmail.com>
2022-01-24 20:31:03 +0000

    added a script to do 19_30

---
+++ LocalCodeGeneratorYichengpiRNA19_30.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Run these codes in the current SERVER directory
+#it generates three testfiles: testcode, testFolderPath and testSampleList
+#usage: ~/programs/LocalCodeGeneratorYichengpiRNA.sh test mm9 36 SE
+#but in fact hard trimming won't be performed. See line 15
+#the file test has two columns, Full Location Path and name, only one space allowed
+#this script enables combining multiple flowcells.
+
+/woldlab/castor/home/phe/programs/Cleanup.sh ATAC
+
+echo '' > testcode
+CurrentLo=$(pwd)
+
+source /woldlab/castor/home/phe/programs/GenomeDefinitions.sh $2
+source /woldlab/castor/home/phe/programs/Localrefolder.sh $1 $2 0 $4 19 30
+
+/woldlab/castor/home/phe/programs/BowtieYichengCodeGeneratorsmallRNA.sh testFolderPath $2 19_30 $4
+
+/woldlab/castor/home/phe/programs/bigWigCode.sh testFolderPath $2 "19_30mer"
+
+/woldlab/castor/home/phe/programs/TrackSummary.sh bigWig
