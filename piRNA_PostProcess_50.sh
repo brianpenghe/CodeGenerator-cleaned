6f1ee0f40f8adc47f1a44bf502ac92fba2e21a83
brianpenghe <brianpenghe@gmail.com>
2022-05-04 21:53:25 +0100

    added a script to calculate bin counts

---
+++ piRNA_PostProcess_50.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#bin counts for vector mappings
+Coordinates_20A='chrX:21519548:21560880'
+Coordinates_42AB='chr2R:6256844:6499214'
+Coordinates_Flam='chrX:21632639:21883809'
+
+ls *vectoronly*.bam | rev | cut -d. -f2- | rev > bams
+for i in 10 100 1000
+  do
+    while read bam
+      do
+        bamCoverage -b $bam.bam \
+-of bedgraph -bs $i -o $bam.$i.bg4
+        bamCoverage -b $bam.bam \
+-of bedgraph -bs $i --samFlagInclude 16 -o $bam.$i.Minus.bg4
+        bamCoverage -b $bam.bam \
+-of bedgraph -bs $i --samFlagExclude 16 -o $bam.$i.Plus.bg4
+      done<bams
+  done
+
+#bin counts for genome region mappings
+ls *50mer.unique*.bam > bamsgenome
+for i in 10 100 1000
+  do
+    while read bam
+      do
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_20A -o $bam.$i.20A.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_20A --samFlagInclude 16 -o $bam.$i.20A.Minus.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_20A --samFlagExclude 16 -o $bam.$i.20A.Plus.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_42AB -o $bam.$i.42A.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_42AB --samFlagInclude 16 -o $bam.$i.42A.Minus.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_42AB --samFlagExclude 16 -o $bam.$i.42A.Plus.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_Flam -o $bam.$i.flamenco.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_Flam --samFlagInclude 16 -o $bam.$i.flamenco.Minus.bg4
+        bamCoverage -b $bam \
+-of bedgraph -bs $i --region $Coordinates_Flam --samFlagExclude 16 -o $bam.$i.flamenco.Plus.bg4
+      done<bamsgenome
+  done
+
+#chop to retrieve empty bins
+for i in 10 100 1000
+  do ls *.$i.*bg4 > bg4.$i.list
+    while read bg4
+      do
+        awk -vOFS="\t" '{ print $1, $2, $3, ".", $4 }' $bg4 > signal.bed
+        bedops --chop $i signal.bed | bedmap --echo --echo-map-score - signal.bed | \
+sed -e 's/|/\t/g' > $bg4.chopped.bg4
+      done<bg4.$i.list
+  done
+
+#make bigWig
+
+while read bam
+  do
+    bamCoverage -b $bam \
+-of bigwig -bs 1 --samFlagInclude 16 -o $bam.1.Minus.bigWig
+    bamCoverage -b $bam \
+-of bigwig -bs 1 --samFlagExclude 16 -o $bam.1.Plus.bigWig
+done<<<$(ls *.dm?.50mer.unique.dup.bam)
