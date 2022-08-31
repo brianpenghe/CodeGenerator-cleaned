a2e1331f45741449325f1f1eb1220ed192e6502f
brianpenghe <brianpenghe@gmail.com>
2022-08-31 17:44:37 +0100

    fixed the python version and script path

--- CoverageBigWig.sh
+++ CoverageBigWig.sh
@@ -5,6 +5,6 @@
 uniquewig=$(echo -e $1"Signal.Unique.str1.out.bg")
 allwig=$(echo -e $1"Signal.UniqueMultiple.str1.out.bg")
 
-python /woldlab/castor/home/sau/code/gene_coverage_wig_gtf.py $2 $uniquewig 1000 $1".coverage"
+python2 /woldlab/castor/home/sau/code/GeorgiScripts/gene_coverage_wig_gtf.py $2 $uniquewig 1000 $1".coverage"
 /woldlab/castor/proj/programs/x86_64/wigToBigWig -clip $uniquewig $3 $uniquewig.bigWig
 /woldlab/castor/proj/programs/x86_64/wigToBigWig -clip $allwig $3 $allwig.bigWig
