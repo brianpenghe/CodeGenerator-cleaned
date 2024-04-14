2255aa89e2c6289cb087d26d3382613f8f395545
brianpenghe <brianpenghe@users.noreply.github.com>
2024-04-14 21:28:36 +0100

    added 2kbCirceSDGFP BowtieYichengCodeGeneratorRNA.sh

--- BowtieYichengCodeGeneratorRNA.sh
+++ BowtieYichengCodeGeneratorRNA.sh
@@ -5,7 +5,7 @@
 #usage: ./bowtieCodeGenerator.sh testFolderPath dm3/dm6 23_29
 CurrentLo=$(pwd)
 source ~/programs/GenomeDefinitions.sh $2
-plasmids=( 'UBIG' 'MIMIC' 'originalUBIG' 'UBIGasG' 'Dmel_Circe' 'drorep')
+plasmids=( 'UBIG' 'MIMIC' 'originalUBIG' 'UBIGasG' 'Dmel_Circe' 'drorep' '2kbCirceSDGFP')
 
 echo "#!/bin/bash" >> testcodePostBowtie
 echo "#!/bin/bash" >> testcodePostBowtie2
