b103f633152c8e001b3530c5cd3b95857f704c20
brianpenghe <brianpenghe@users.noreply.github.com>
2024-04-14 21:29:46 +0100

    added 2kbCirceSDRallmutGFP

--- BowtieYichengCodeGeneratorsmallRNA.sh
+++ BowtieYichengCodeGeneratorsmallRNA.sh
@@ -10,7 +10,8 @@ plasmids=( '42AB_UASG' '42AB_UBIG' '66A_UASG' '66A_UBIG' 'ACTIN-G' 'GFP_SV40' \
 'originalUBIG' 'UBIGasG' 'UASMhc17GFP' \
 'LNLB' 'UBIGBox' 'Dmel_Circe' 'PlacW' \
 'MIMIC' '2kbCirceSDGFP' 'ActinSDSAGFP' '2kbCirceSDSAGFP' \
-'NosBxLuciBxNosAS' 'NosLuciASBx' 'UASPmk2K10' 'UASmk2BxBUASas' 'drorep')
+'NosBxLuciBxNosAS' 'NosLuciASBx' 'UASPmk2K10' 'UASmk2BxBUASas' 'drorep' \
+'2kbCirceSDRallmutGFP')
 
 echo "#!/bin/bash" >> testcodePostBowtie
 echo "#!/bin/bash" >> testcodePostBowtie2
